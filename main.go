6c54d5c69b44647d03c89a280625da6a80d3ccb1
gus <gustavo_9_rj@hotmail.com>
2022-05-31 05:23:28 +0200

    ya le el .env

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // // album represents data about a record album.
@@ -29,8 +32,9 @@ func getAPI(c *gin.Context) {
 
 func getGraphQL(c *gin.Context) {
 	body, _ := ioutil.ReadAll(c.Request.Body)
+	backendUrl := os.Getenv("ERPBACK_URL")
 
-	request, reqErr := http.NewRequest("POST", "http://localhost:9090/graphql", bytes.NewBufferString(string(body)))
+	request, reqErr := http.NewRequest("POST", backendUrl+"graphql", bytes.NewBufferString(string(body)))
 	request.Header.Add("Content-Type", "application/json")
 	if reqErr != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", reqErr)
@@ -59,6 +63,11 @@ func getGraphQL(c *gin.Context) {
 }
 
 func main() {
+	errEnv := godotenv.Load(".env")
+	if errEnv != nil {
+		log.Fatalf("Some error occured. Err: %s", errEnv)
+	}
+
 	router := gin.Default()
 	router.GET("/api", getAPI)
 	router.POST("/graphql", getGraphQL)
